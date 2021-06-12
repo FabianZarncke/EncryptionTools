27b90ea13cf2a6fd8ed6bc0d3c832507eb346eba
FabianZarncke <69311042+FabianZarncke@users.noreply.github.com>
2021-06-12 02:25:14 +0200

    Document Title Feature
    The Document Title now changes depended on what tab is active.
    + Zencryption is now Zencrypt

--- src/index/index.js
+++ src/index/index.js
@@ -1,16 +1,24 @@
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
+//tabLayout functionality
 tabs.forEach(tab =>{
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
+        //remove "active" class from all tabContent's
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('active')
         })
+        //remove "active" class from all tab's
         tabs.forEach(tab => {
             tab.classList.remove('active')
         })
+        //add "active" class to the chosen tab and tabContent
         tab.classList.add('active')
         target.classList.add('active')
+        //set the document title accordingly
+        const str = target.id
+        const str2 = str.charAt(0).toUpperCase() + str.slice(1)
+        document.title = "Zencrypt | " + str2
     })
 })
